e164cc67b436ee1434825f4f0cf6ff2e388629a5
Adam Matejczyk <mateja3144@gmail.com>
2018-08-13 14:16:08 +0200

    Dodanie funkcji download, zamiana <br> na nowe linnie. Ukrycie wygenerowanych aliasków. Program Gotowy

--- generowanie.js
+++ generowanie.js
@@ -1,15 +1,37 @@
-var startReg = 'Windows Registry Editor Version 5.00</br></br>[HKEY_LOCAL_MACHINE\\\SOFTWARE\\\SYSTEM-1]</br></br>[HKEY_LOCAL_MACHINE\\\SOFTWARE\\\SYSTEM-1\\\Aliasy]<br/>"DefaultAlias"=""</br>"AutoDefaultAlias"=dword:00000000</br>';
+var startReg = 'Windows Registry Editor Version 5.00<br/><br/>[HKEY_LOCAL_MACHINE\\\SOFTWARE\\\SYSTEM-1]<br/><br/>[HKEY_LOCAL_MACHINE\\\SOFTWARE\\\SYSTEM-1\\\Aliasy]<br/>"DefaultAlias"=""<br/>"AutoDefaultAlias"=dword:00000000<br/>';
 var export32 = "";
 var export64 = "";
 var lista = "";
-var bazy =["AUDYK", "ALD", "AGILE", "AIRJU", "BUD", "DIALO", "EUROE", "GGAUD", "GRUGU", "GGKAN", "GGCOR", "GLOBA", "LARXX", "MONTV", "ORION", "OGLFO", "OGLSE", "PENZI", "STLXX", "REBAU", "GRX", "RUTRO", "ROLFE", "WHEEL", "TJH2B", "TUMBL", "WRJSE", ]
+var bazy =["AUDYK", "ALD", "AGILE", "AIRJU", "BUD", "DIALO", "EUROE", "GGAUD", "GRUGU", "GGKAN", "GGCOR", "GLOBA", "LARXX", "MONTV", "ORION", "OGLFO", "OGLSE", "PENZI", "STLXX", "REBAU", "GRX", "RUTRO", "ROLFE", "WHEEL", "TJH2B", "TUMBL", "WRJSE", "TEST" ]
 var ilosc = bazy.length - 1;
 
 function genList(){
     for(var i=0; i<=ilosc; i++){
-        lista = lista + '<input id="'+bazy[i]+'" type="checkbox">'+bazy[i]+' </br>  '
+        lista = lista + '<input id="'+bazy[i]+'" type="checkbox">'+bazy[i]+' <br/>  '
     }
     document.getElementById("lista").innerHTML = lista;
+}
+
+function download (data, filename, type) {
+    var file = new Blob([ data ], { type: type });
+ 
+    if (window.navigator.msSaveOrOpenBlob) {
+        return window.navigator.msSaveOrOpenBlob(file, filename);
+    }
+ 
+    var link = document.createElement("a");
+    var url = URL.createObjectURL(file);
+ 
+    link.href = url;
+    link.download = filename;
+    document.body.appendChild(link);
+ 
+    link.click();
+ 
+    setTimeout(function () {
+        document.body.removeChild(link);
+        window.URL.revokeObjectURL(url);  
+    }, 0);
 }
 
 /*function genList () {
@@ -29,10 +51,12 @@ function exportList32(){
     for(var i=0; i<=ilosc; i++){
         var ptaszek = document.getElementById(bazy[i]).checked;
         if(ptaszek == true){
-            export32 = export32 + '</br>[HKEY_LOCAL_MACHINE\\\SOFTWARE\\\SYSTEM-1\\\Aliasy'+"\\"+''+bazy[i]+']</br>"ServerAdres"="192.168.105.207:/home/bazy/'+bazy[i]+'/SYSTEM-1.S1B"</br>';
+            export32 = export32 + '<br/>[HKEY_LOCAL_MACHINE\\\SOFTWARE\\\SYSTEM-1\\\Aliasy'+"\\"+''+bazy[i]+']<br/>"ServerAdres"="192.168.105.207:/home/bazy/'+bazy[i]+'/SYSTEM-1.S1B"<br/>';
         }
      }
-    document.getElementById("wygenerowane").innerHTML = export32;
+    export32 = export32.replace(/<br\/>/g, '\n');
+    download(export32, "aliasy32.reg", "text/plain");
+
 }
 
 function exportList64(){
@@ -40,10 +64,11 @@ function exportList64(){
     for(var i=0; i<=ilosc; i++){
        var ptaszek = document.getElementById(bazy[i]).checked;
        if(ptaszek == true){
-            export64 = export64 + '</br>[HKEY_LOCAL_MACHINE\\\SOFTWARE\\\Wow6432Node\\\System-1\\\Aliasy'+"\\"+''+bazy[i]+']</br>"ServerAdres"="192.168.105.207:/home/bazy/'+bazy[i]+'/SYSTEM-1.S1B"</br>';
+            export64 = export64 + '<br/>[HKEY_LOCAL_MACHINE\\\SOFTWARE\\\Wow6432Node\\\System-1\\\Aliasy'+"\\"+''+bazy[i]+']<br/>"ServerAdres"="192.168.105.207:/home/bazy/'+bazy[i]+'/SYSTEM-1.S1B"<br/>';
        }                                           
     }
-    document.getElementById("wygenerowane").innerHTML = export64;
+    export64 = export64.replace(/<br\/>/g, '\n');
+    download(export64, "aliasy64.reg", "text/plain");
 }
 
 
